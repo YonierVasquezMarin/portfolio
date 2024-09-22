00f297781e54ca14a436b5bc3717dc729fb36c85
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-21 21:56:09 -0500

    Iterative list was added to home

--- src/screens/app/app.component.ts
+++ src/screens/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgFor],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio';
+    skills = [
+        'Teamwork',
+        'Problem solving',
+        'Continuous learning',
+        'Agile methodologies',
+        'Web development',
+        'Mobile development',
+        'APIs',
+        'Version control',
+        'Code review',
+        'CI/CD',
+        'DevOps',
+        'Cloud computing',
+        'UX/UI',
+        'Performance',
+        'Responsive design',
+        'Scalability',
+        'Microservices',
+        'Machine learning',
+    ]
 }
